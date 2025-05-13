4439647d39d74daec4b18566f33945873aaec37a
moseyda <moseyda20@gmail.com>
2025-05-13 02:41:03 +0100

    updates
    menu bar changes made

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,19 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navbarLinks = document.querySelector('.navbar-links');
 
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const sidebar = document.getElementById('sidebar');
-const closeBtn = document.getElementById('close-btn');
-
-
-hamburgerMenu.addEventListener('click', () => {
-
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0'; 
-    } else {
-        sidebar.style.width = '250px';
-    }
-});
-
-
-closeBtn.addEventListener('click', () => {
-    sidebar.style.width = '0';
+hamburger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+    hamburger.classList.toggle('active'); 
 });
